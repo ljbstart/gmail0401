8ba203082fafa1f4f06f5b889245097af8aa0d95
lijianbo <jinaboli72@foxmail.com>
2019-12-03 16:22:11 +0800

    加入用户管理模块

---
+++ gmail0401-user-manage/src/main/java/com/shou/gmail0401/user/Gmail0401UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.shou.gmail0401.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmail0401UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmail0401UserManageApplication.class, args);
+    }
+
+}
